766e1f68b1772a8b6c8d7631e13cc9d6c5bb8e08
Isac Sund <isac.sund@outlook.com>
2020-05-24 12:54:21 +0300

    Separate extracting into own function

--- src/main.rs
+++ src/main.rs
@@ -17,46 +17,49 @@ fn run() {
         }
         ("extract", Some(extract_matches)) => {
             let fname = extract_matches.value_of("archive").unwrap();
-
             let format = ArchiveFormat::get_format_from_archive(fname).unwrap();
-            match format {
-                ArchiveFormat::Rar => {
-                    let archive = rar::Archive::extract_all(fname, ".", "").unwrap();
-                    for i in 0..archive.files.len() {
-                        println!("File {} extracted,", archive.files[i].name);
-                    }
-                }
-                ArchiveFormat::Zip => {
-                    let fname = std::path::Path::new(fname);
-                    let file = fs::File::open(&fname).unwrap();
-                    let mut archive = zip::ZipArchive::new(file).unwrap();
-                    for i in 0..archive.len() {
-                        let mut file = archive.by_index(i).unwrap();
-                        let outpath = file.sanitized_name();
 
-                        {
-                            let comment = file.comment();
-                            if !comment.is_empty() {
-                                println!("File {} comment: {}", i, comment);
-                            }
-                        }
-
-                        println!(
-                            "File {} extracted to \"{}\"",
-                            i,
-                            outpath.as_path().display()
-                        );
-
-                        let mut outfile = fs::File::create(&outpath).unwrap();
-                        io::copy(&mut file, &mut outfile).unwrap();
-                    }
-                }
-            }
+            extract(format, fname);
         }
         _ => eprintln!("No command was provided"),
     }
 }
 
+fn extract(format: ArchiveFormat, fname: &str) {
+    match format {
+        ArchiveFormat::Rar => {
+            let archive = rar::Archive::extract_all(fname, ".", "").unwrap();
+            for i in 0..archive.files.len() {
+                println!("File {} extracted,", archive.files[i].name);
+            }
+        }
+        ArchiveFormat::Zip => {
+            let fname = std::path::Path::new(fname);
+            let file = fs::File::open(&fname).unwrap();
+            let mut archive = zip::ZipArchive::new(file).unwrap();
+            for i in 0..archive.len() {
+                let mut file = archive.by_index(i).unwrap();
+                let outpath = file.sanitized_name();
+
+                {
+                    let comment = file.comment();
+                    if !comment.is_empty() {
+                        println!("File {} comment: {}", i, comment);
+                    }
+                }
+
+                println!(
+                    "File {} extracted to \"{}\"",
+                    i,
+                    outpath.as_path().display()
+                );
+
+                let mut outfile = fs::File::create(&outpath).unwrap();
+                io::copy(&mut file, &mut outfile).unwrap();
+            }
+        }
+    }
+}
 enum ArchiveFormat {
     Zip,
     Rar,
