62401fb4b25f0479479ccdf2926cae9c052ee333
Isac Sund <isac.sund@outlook.com>
2020-05-25 20:59:05 +0300

    Move commands into cmd folder

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ extern crate clap;
 
 extern crate tap;
 
-mod extract;
-
 use tap::app;
 
+mod cmd;
+
 fn main() {
-    let app = app::new().subcommand(extract::command());
+    let app = app::new().subcommand(cmd::extract::command());
 
     app::parse_and_process_subcommand(app, |name, args| match name {
-        extract::COMMAND_NAME => extract::handler(args),
+        cmd::extract::COMMAND_NAME => cmd::extract::handler(args),
         _ => Err("Unknown subcommand".into()),
     })
 }
